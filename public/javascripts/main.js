0018aacc22e73f57cf723458c8aa8f27a0c841ae
alejandroolmed59 <00097017@uca.edu.sv>
2018-12-01 16:35:19 -0600

    Anniadido boton con evento delete a cada registro de la tabla

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -10,15 +10,32 @@ let app = {
         //console.log(element);
         var tbody = document.getElementsByClassName('tablaALlenar')[0];
         var tr = document.createElement('tr');
-        tr.innerHTML = `
-            <td>${element._id}</td>
-            <td>${element.Titulo}</td>
-            <td>${element.Desarrolladora}</td>
-            <td>${element.Ign}</td>
-            `;
+        tr.innerHTML = `<td></td>
+                        <td>${element._id}</td>
+                        <td>${element.Titulo}</td>
+                        <td>${element.Desarrolladora}</td>
+                        <td>${element.Ign}</td>
+                        <td>
+                            <a class="delete"><i class="fas fa-ban"></i>
+                            <a class="actualizar"><i class="fas fa-pencil-alt"></i>
+                        </td>`;
+        tr.getElementsByClassName('delete')[0].addEventListener("click", (event) => {
+            this.delete(event, element, tr, tbody);
+        });
         tbody.appendChild(tr);
     },
-
+    delete: function (event, element, tr, tbody) {
+        event.preventDefault();
+        fetch('/' + element._id, {
+            method: 'DELETE'
+        })
+            .then(res => {
+                return res.json();
+            })
+            .then(element =>  {
+                tbody.removeChild(tr);
+            });
+    },
     loadContent: function () {
         fetch('/api/Videojuegos', {
             method: 'GET'
